217079bde3d9345e8765758ab2d4f44e7a254ca5
Eugenia <meg19831@hotmail.com.ar>
2022-08-17 14:11:53 -0300

    Creando repositorio con carpetas

---
+++ clase1/index.js
@@ -0,0 +1,2 @@
+let entrada = prompt ( `ingrese su nombre`);
+let salida = alert ( `Bienvenida/o a esta aventura` + " " + entrada + " " + "pronto con mas novedades..." );
---
+++ clase2/index.js
@@ -0,0 +1,19 @@
+
+let entrada = prompt ("Ingrese su nombre");
+let apellido = prompt ("Ingrese su apellido");
+
+alert ( `su nombre completo es` + " " +  entrada + " " + apellido);
+let jugadores = alert ("Nos divideremos en grupos para poder armar los grupos");
+let edad = prompt ("Ingrese su edad");
+
+if (edad <=14) {
+    alert("Eres menor de edad para participar de este juego");
+}if (edad >= 15 && edad <=20 ) {
+    alert("formaras parte del equipo rojo");
+} if(edad >= 21 && edad <=26 ) {
+    alert ("formaras parte del equipo azul");
+} if (edad >= 27 && edad <=31 ) {
+    alert ("formaras parte del equipo verde");
+}else if (edad >= 32) {
+    alert ("Eres mayor de la edad permitida para este juego, paticiparas en la siguiente ronda");
+}
