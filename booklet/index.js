8749440778aec9082fb7b8fe17bab05d2c9a82d9
Eugenia <meg19831@hotmail.com.ar>
2022-08-21 14:25:02 -0300

    se creo carpeta de Booklet y archivo html y js

---
+++ booklet/index.js
@@ -0,0 +1,43 @@
+//Actividad 1 Homero
+
+/* let nombre= "Homero";
+let apellido= "Simpson";
+let edad= 39;
+
+//Actividad 2 Ciudades
+
+const ciudad1= "Buenos Aires";
+const ciudad2 = "Catamarca";
+const ciudad3 = "La Rioja";
+const ciudad4 = "Santa Fe";
+const ciudad5 = "Bariloche";
+
+console.log(ciudad1);
+console.log(ciudad2); */
+
+
+//Actividad 3 Carnet
+
+/* let ciudad ="Springfiel";
+let tipoLicencia= "Driver License";
+let matricula= "B47u89re243";
+let conductor= "Bart Simpson";
+let direccion= "742 Evergreen terr";
+let pais= "Spingfiel USA";
+
+
+let carnet=  ciudad + conductor + direccion;
+console.log(carnet);
+ */
+
+//actividad 4 Registro de Personas
+
+let integrante= alert ("Deberas ingresar el nombre de una familia con 5 integrantes");
+let integrante1= prompt("Integrante 1");
+let integrante2= prompt("Integrante 2");
+let integrante3= prompt("Integrante 3");
+let integrante4= prompt("Integrante 4");
+let integrante5= prompt("Integrante 5");
+
+let familia =  alert ( `los integrantes ingresados son:`  + integrante1 + " " + integrante2+ " " + integrante3+ " " + integrante4 + " " + integrante5 );
+
