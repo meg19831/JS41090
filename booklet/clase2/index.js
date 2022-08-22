4d67343a5156e68f6e2fa8f968fe8e262af1379a
Eugenia <meg19831@hotmail.com.ar>
2022-08-22 18:23:00 -0300

    agregando carpata de booklet y carpeta desafios

---
+++ booklet/clase2/index.js
@@ -0,0 +1,35 @@
+//actividad 1 js "Yo no fui"
+
+/* let nombre = prompt ("ingrese su nombre");
+if (nombre == "Roberto") {
+    alert ("Fui yo");
+}
+else {
+    alert ( "Yo no fui")
+} */
+
+// actividad 2 js presionar Y
+
+/* let usuario = prompt ("Ingrese una letra ");
+if ((usuario == "Y") || (usuario =="y")) {
+    alert ("Correcto");
+} else {
+    alert ("Incorrecto");
+}
+ */
+//actividad 3 escoger personaje
+
+let usuario = prompt ("Ingrese un numero");
+
+
+if (usuario == 1){
+    alert ("Escogiste a Marge");
+}if (usuario== 2) {
+    alert ("Escogiste a Bart");
+} if (usuario == 3){
+    alert ("Escogiste a Magui");
+} if (usuario == 4){
+    alert ("Escogiste a Homero");
+} else if (usuario == 5){
+    alert ("Escogiste a Lisa");
+} 
